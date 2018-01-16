7d2870c8856a7fd17ea2995d57d5530efb363e92
Shintaro Tanaka <sin_tanaka@jsl.co.jp>
2018-01-16 10:54:58 +0900

    initial commit.

---
+++ src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="app">
+    {{ msg }}
+    <div>
+      send a request:
+        <button @click=post()>POST</button>
+        <input type='text' v-model="url">
+        <pre>
+          {{ resp | json}}
+        </pre>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      url: 'http://localhost:8000/api/v1/mentors/',
+      resp: '',
+    }
+  },
+  methods: {
+    post: function () {
+      axios.get(this.url)
+        .then((response) => {
+            this.resp = response
+            console.log(response)
+        })
+        .catch((error)=>{
+            this.resp = error
+            console.log(error)
+        })
+    }
+  },
+  filters: {
+    json: value => { return JSON.stringify(value, null, 2)},
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  // text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
